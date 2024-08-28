7f688f5fd396cf770df3d1c6f8c47f1520e2599a
sam <sfriedman6711@gmail.com>
2024-08-28 19:54:18 -0400

    able to get table into pandas df

--- scrap.py
+++ scrap.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import os
 
 url = "https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue"
 
@@ -8,9 +9,27 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.text, "html")
 
+cwd = os.getcwd()
+path = cwd + "/new.csv"
 
-a = soup.find_all("table")[1]
+table = soup.find_all("table")[0]
 
-b = a.find_all("th")
+table_titles = table.find_all("th")[1:9]
 
-print(b)
+column_titles = [title.text.strip() for title in table_titles]
+
+# print(column_titles)
+
+df = pd.DataFrame(columns=column_titles)
+
+column_data = table.find_all("tr")
+
+
+for r in column_data[2:]:
+    row_data = r.find_all("td")
+    row = [data.text.strip() for data in row_data]
+    # print(row)
+    length = len(df)
+    df.loc[length] = row
+
+df.to_csv(path)

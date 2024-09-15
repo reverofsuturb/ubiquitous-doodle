270475c6e0eec2cef57533f4ab271dd12fa26340
sam <sfriedman6711@gmail.com>
2024-09-14 22:30:02 -0400

    setting up soup

--- scrap.py
+++ scrap.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import os
 
-url = "https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue"
+url = "https://poe.ninja/economy/settlers/skill-gems?level=1&gemType=Transfigured"
 
 page = requests.get(url)
 
@@ -12,24 +12,25 @@ soup = BeautifulSoup(page.text, "html")
 cwd = os.getcwd()
 # path = cwd + "/new.csv"
 
-table = soup.find_all("table")[0]
+table = soup.find_all("table")
 
 table_titles = table.find_all("th")[1:9]
 
 column_titles = [title.text.strip() for title in table_titles]
 
-# print(column_titles)
 
-df = pd.DataFrame(columns=column_titles)
+print(column_titles)
 
-column_data = table.find_all("tr")
+# df = pd.DataFrame(columns=column_titles)
+
+# column_data = table.find_all("tr")
 
 
-for r in column_data[2:]:
-    row_data = r.find_all("td")
-    row = [data.text.strip() for data in row_data]
-    # print(row)
-    length = len(df)
-    df.loc[length] = row
+# for r in column_data[2:]:
+#     row_data = r.find_all("td")
+#     row = [data.text.strip() for data in row_data]
+#     # print(row)
+#     length = len(df)
+#     df.loc[length] = row
 
-df.to_csv(path)
+# df.to_csv(path)

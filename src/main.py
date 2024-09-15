52c95bce588c955a4e7aef74d78279ad39be4360
sam <sfriedman6711@gmail.com>
2024-09-14 22:24:25 -0400

    base bot

---
+++ src/main.py
@@ -0,0 +1,62 @@
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+# loading discord token
+load_dotenv()
+TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
+print(TOKEN)
+
+# intents are permissions the bot needs to see and respond to messages
+intents: Intents = Intents.default()
+intents.message_content = True
+
+# represents the client connection and assigns our intents/permissions
+client: Client = Client(intents=intents)
+
+
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print("(Message was empty because of lack of permissions/intents)")
+        return
+
+    if is_private := user_message[0] == "?":
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        (
+            await message.author.send(response)
+            if is_private
+            else await message.channel.send(response)
+        )
+    except Exception as e:
+        print(e)
+
+
+@client.event
+async def on_ready() -> None:
+    print(f"{client.user} is now running!")
+
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}')
+    await send_message(message, user_message)
+
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+
+if __name__ == "__main__":
+    main()
